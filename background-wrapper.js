468acb1caacfd7f417d030b81b7fc0b37936461b
Anshuman-Arun <anshumanarun2008@gmail.com>
2026-09-25 21:26:16 -0700

    feat(wave2): load reliability runtime in service worker

--- background-wrapper.js
+++ background-wrapper.js
@@ -1,3 +1,3 @@
 "use strict";
 
-importScripts("shared.js", "lifecycle.js", "background.js", "wave1-core.js", "wave1-store.js", "wave1-background.js", "portability.js", "data-background.js", "tab-supervisor.js");
+importScripts("shared.js", "lifecycle.js", "background.js", "wave1-core.js", "wave1-store.js", "wave1-background.js", "wave2-core.js", "wave2-db.js", "wave2-delivery-store.js", "wave2-result-store.js", "wave2-store.js", "wave2-faults.js", "wave2-background.js", "portability.js", "data-background.js", "tab-supervisor.js");
